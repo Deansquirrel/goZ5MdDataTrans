bb583c25ee503cdbd78c3250cf1bcdde372d5399
yuanxs <yuanxs@95szzl.cn>
2019-08-20 15:47:27 +0800

    201908201547

--- global/Global.go
+++ global/Global.go
@@ -6,9 +6,9 @@ import (
 )
 
 const (
-	//PreVersion = "0.0.0 Build20190101"
+	//PreVersion = "1.0.1 Build20190820"
 	//TestVersion = "0.0.0 Build20190101"
-	Version   = "1.0.1 Build20190820"
+	Version   = "0.0.0 Build20190101"
 	Type      = "Z5MdDataTrans"
 	SecretKey = "Z5MdDataTrans"
 )
